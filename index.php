5892ab918cc9395c60f6abb2eefb39e5d971776f
Dev-Patel-09 <devrpatel0914@gmail.com>
2025-07-10 11:21:30 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <h1>This app is build by Dev patel</h1>
+    <?php
+    echo "<h1>This app is created by dev patel and using azure app service</h1>";
+    ?>
+    <a href="about.php">About us</a>
+  </body>
+</html>
